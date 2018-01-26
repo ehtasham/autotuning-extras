0b7af061734ad8e3f8fbb43f88bfc431fb95712a
ehtasham <ehtashamawan@gmail.com>
2018-01-26 17:28:59 +0500

    new files

---
+++ extractingconfigs.py
@@ -0,0 +1,20 @@
+a=[{"3032080": {"0.003985166549682617": {"-assumption-cache-tracker": "off", "-dse": "on", "-ipsccp": "off", }}}]
+# print a
+# print a[0]
+# print a[0].values()[0].values()[0].values()
+
+#create directory
+# import os
+# directory ='../build'
+# try: 
+#     os.makedirs(path)
+# except OSError:
+#     if not os.path.isdir(path):
+#         raise
+
+import sys
+if len(sys.argv) == 3:
+	print "two arguments"
+else:
+	print "not"
+# print len(sys.argv) 
